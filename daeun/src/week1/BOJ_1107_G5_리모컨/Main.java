96df57271ee96225831d5bf6788b082e0a3f59ec
강다은 <da010228@naver.com>
2023-02-04 11:47:17 +0900

    fix : BOJ 1107 G5 리모컨

--- daeun/src/week1/BOJ_1107_G5_리모컨/Main.java
+++ daeun/src/week1/BOJ_1107_G5_리모컨/Main.java
@@ -1,37 +1,43 @@
 package week1.BOJ_1107_G5_리모컨;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-    static boolean[] broken = new boolean[10];
-    static int N,M;
-    static int limit,min;
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        N=sc.nextInt();
-        M=sc.nextInt();
-        for(int i=0;i<M;i++){
-            broken[sc.nextInt()]=true;
-        }
-        limit = (N+"").length()+1;
-        min = Math.abs(N - 100);
-        for (int i = 0; i < 10; i++){
-            if(broken[i]){
-                continue;
-            }
-            solve(1,i);
-        }
-        System.out.println(min);
-    }
-    static void solve(int cnt, int ch) {
-        if(limit < cnt){
-            return ;
-        }
-        min = Math.min(min, cnt + Math.abs(N - ch));
-        for (int i = 0; i < 10; i++){
-            if(broken[i]){
-                continue;
-            }
-            solve(cnt+1, (ch * 10) + i);
-        }
-    }
+
+	static int N, M, min, over;
+	static boolean[] broken  = new boolean[10];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		over = Integer.toString(N).length()+1;
+		M = Integer.parseInt(br.readLine()); 
+		if(M>0) {
+			String[] split = br.readLine().split(" ");
+			for(int i = 0;i<split.length;i++) {
+				broken[Integer.parseInt(split[i])] = true;
+			}
+		}
+		min = Math.abs(N-100);
+		
+		for(int i=0;i<10;i++) {
+			if(broken[i]) {
+				continue;
+			}
+			solve(1, i);
+		}
+		System.out.println(min);
+	}
+	private static void solve(int count, int channel) {
+		if(over<count) { 
+			return;
+		}
+		min = Math.min(min, count+Math.abs(N-channel));
+		for(int i=0;i<10;i++) {
+			if(broken[i]) {
+				continue;
+			}
+			solve(count+1, channel*10 + i);
+		}
+	}
 }
