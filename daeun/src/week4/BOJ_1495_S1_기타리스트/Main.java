e14fecf9c8e8cb7d070d02e912c319a0eca6e2a9
강다은 <da010228@naver.com>
2023-02-24 22:50:11 +0900

    fix: BOJ 1495 S1 기타리스트

--- daeun/src/week4/BOJ_1495_S1_기타리스트/Main.java
+++ daeun/src/week4/BOJ_1495_S1_기타리스트/Main.java
@@ -2,67 +2,50 @@ package week4.BOJ_1495_S1_기타리스트;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
-
 	private static int n, s, m, max;
-	private static int[] vol, now;
+	private static int vol[], visit[][];
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
 		String[] nsm = br.readLine().split(" ");
-		n = Integer.parseInt(nsm[0]); // 곡의 개수
-		s = Integer.parseInt(nsm[1]); // 시작 볼륨
-		m = Integer.parseInt(nsm[2]); // 최대 볼륨
+		n = Integer.parseInt(nsm[0]);
+		s = Integer.parseInt(nsm[1]);
+		m = Integer.parseInt(nsm[2]);
 		vol = new int[n+1];
 		max = -1;
 		String[] split = br.readLine().split(" ");
+		
+		vol[0] = s;
 		for (int i = 1; i < n+1; i++) {
 			vol[i] = Integer.parseInt(split[i-1]);
 		}
-		//현재볼륨 +- vol[i] (1~m 사이)
 
-		int plus, minus;
-		now = new int[m+1]; 
-		now[s] = 0; //볼륨 S는 0번째 노래 (시작)
-		ArrayList<Integer> list = new ArrayList<>();
-		ArrayList<Integer> list2 = null;
-		list.add(s);
-		for(int i=1;i<n+1;i++){
-			list2 = new ArrayList<>();
-			for(int a : list) {
-				plus = a + vol[i];
-				minus = a - vol[i];
-				/*if(plus> m && minus < 0) {
-					max = -1;
-					break;
-				}*/
-				if(plus <= m) {
-					now[plus] = i;
-					list2.add(plus);
-				}
-				if(minus >= 0) {
-					now[minus] = i;
-					list2.add(minus);
-				}		
-			}
+		visit = new int[n+2][m+1];
+		recursion(1,s);
 		
-			if(list2.size() == 0) {
-				max = -1;
-				break;
-			}
-			
-			
-			if(i==n) {
-				for(int a: list2) {
-					max = Math.max(a, max);
-				}
-			}
-			
-			list = list2;
-		}
-				
 		System.out.println(max);
 	}
+	
+	private static void recursion(int start, int sum) {
+		if(start == n+1) {
+			max = Math.max(max, sum);
+			return;
+		}
+		visit[start][sum] = 1;
+		int plus = sum + vol[start];
+		int minus = sum - vol[start];
+		if(plus > m && minus < 0) {
+			visit[start][sum] = -1;
+			return;
+		}
+		if(plus<=m && visit[start+1][plus]!=1) { 
+			recursion(start+1, plus);
+		}
+		if(minus>=0 && visit[start+1][minus]!=1) {
+			recursion(start+1, minus);
+		}
+		return;
+	}	
 }
