8362e8027c32dc86501b55e6bd1a502b68eafba7
daeun kang <69593799+da010228@users.noreply.github.com>
2023-06-20 20:10:07 +0900

    fix : BOJ 17836 G5 공주님을 구해라

--- daeun/src/week18/BOJ_17836_G5_공주님을구해라/Main.java
+++ daeun/src/week18/BOJ_17836_G5_공주님을구해라/Main.java
@@ -10,33 +10,29 @@ public class Main {
 		String[] split = br.readLine().split(" ");
 		int N = Integer.parseInt(split[0]);
 		int M = Integer.parseInt(split[1]);
-		int T = Integer.parseInt(split[2]); //저주의 제한 시간
-		
+		int T = Integer.parseInt(split[2]);
 		int[][] map = new int[N][M];
-		boolean[][] visit = new boolean[N][M];
+		boolean[][][] visit = new boolean[N][M][2];
 		for(int i=0;i<N;i++) {
 			split = br.readLine().split(" ");
 			for(int j=0;j<M;j++) {
 				map[i][j] = Integer.parseInt(split[j]);
 			}
-		} //입력 끝
+		}
 		int[] dx = {0,0,-1,1}, dy = {-1,1,0,0};
-		
 		Queue<int[]> queue = new ArrayDeque<>();
-		queue.offer(new int[] {0,0});
-		visit[0][0] = true;
-		
+		queue.offer(new int[] {0,0,0});
+		visit[0][0][0] = true;
 		int time = 0;
-		boolean weapon = false;
 		boolean flag = false;
-		
 		out:
-		while(time < T) { //T시간 이내
+		while(time <= T) {
 			int size = queue.size();
 			for(int i=0;i<size;i++) {
 				int[] now = queue.poll();
 				int X = now[0];
 				int Y = now[1];
+				int weapon = now[2];
 				if(X==N-1 && Y==M-1) {
 					flag = true;
 					break out;
@@ -44,25 +40,24 @@ public class Main {
 				for(int d=0; d<4; d++) {
 					int x = X+ dx[d];
 					int y = Y + dy[d];
-					if(x<0 || x>=N || y<0 || y>= M || visit[x][y]) {
+					if(x<0 || x>=N || y<0 || y>= M || visit[x][y][weapon]) {
 						continue;
 					}
 					//빈 공간 이거나, 벽인데 무기가 있거나
-					if(map[x][y] == 0 || (map[x][y] == 1 && weapon)) {
-						queue.offer(new int[] {x, y});
-						visit[x][y] = true;
+					if(map[x][y] == 0 || (map[x][y] == 1 && weapon == 1)) {
+						queue.offer(new int[] {x, y, weapon});
+						visit[x][y][weapon] = true;
 					}
 					//무기거나
 					else if(map[x][y] == 2) {
-						weapon = true;
-						queue.offer(new int[] {x, y});
-						visit[x][y] = true;
+						queue.offer(new int[] {x, y, 1});
+						visit[x][y][0] = true;
+						visit[x][y][1] = true;
 					}
 				}
 			}
 			time++;
 		}
-		
 		if(flag) {
 			System.out.println(time);
 		}
