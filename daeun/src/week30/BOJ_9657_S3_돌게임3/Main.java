d440a40ef796453a8ba6f61e2921d867306bb14a
daeun kang <69593799+da010228@users.noreply.github.com>
2023-09-11 15:00:36 +0900

    code : BOJ S3 9657 돌 게임 3

---
+++ daeun/src/week30/BOJ_9657_S3_돌게임3/Main.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((N % 7 == 0 || N % 7 == 2) ? "CY" : "SK");
+    }
+}
